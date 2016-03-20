5d1a45b3b87080175dd1c13e9e2151459fd2b888
terryso <suchuanyi@126.com>
2016-03-20 22:06:32 +0800

    增加通过公众号第三方平台获取授权信息的例子

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -4,7 +4,12 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
+
 urlpatterns = patterns('',
     url(r'^wechat/', 'echo.views.wechat'),
     url(r'^auth_event/', 'echo.views.auth_event'),
+    url(r'^component_access_token/$', 'echo.views.component_access_token_api'),
+    url(r'^pre_auth_code/$', 'echo.views.pre_auth_code_api'),
+    url(r'^authorize/$', 'echo.views.authorize'),
+    url(r'^save_authorization_info/$', 'echo.views.save_authorization_info', name='save_authorization_info'),
 )
